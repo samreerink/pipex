a4d4ddb549f3a2744820c74ff3a42f2aaed12e73
Sam Reerink <samreerink@gmail.com>
2023-04-17 23:44:53 +0200

    CMD's are running, redirecting still needs to be done

--- pipex.c
+++ pipex.c
@@ -1,39 +1,60 @@
 #include	"pipex.h"
 #include	<stdio.h>
 #include	<stdlib.h>
-
-char	**ft_split(char const *s, char c);
-char	*ft_strjoin(char const *s1, char const *s2);
-char	*ft_strjoin_free(char *s1, char *s2);
-void	*ft_calloc(size_t count, size_t size);
-char	*ft_strdup(const char *s1);
+#include	<unistd.h>
 
 char	*find_cmd_path(char *cmd, char *envp[])
 {
 	size_t	i;
+	int	access_check;
 	char	**path_arr;
 
 	i = 0;
+	access_check = 1;
 	cmd = ft_strjoin("/", cmd);
 	while (!find_path_env(envp[i]))
 		i++;
 	path_arr = ft_split(envp[i] + 5, ':');
 	i = 0;
-	while (path_arr[i] != '\0')
+	while (access_check != 0 && path_arr[i] != '\0')
 	{
-		printf("%s\n", path_arr[i]);
 		path_arr[i] = ft_strjoin_free(path_arr[i], cmd);
-		printf("%s\n", path_arr[i]);
-		i++;
+		access_check = access(path_arr[i], F_OK);
+		if (access_check == -1)
+			i++;
 	}
-	return ("Hoi");
+	return (path_arr[i]);
+}
+
+void	pipex_process(int *pipefd, char *cmd, char *envp[])
+{
+	char	*cmd_path;
+	char	**arg_arr;
+
+	arg_arr = ft_split(cmd, ' ');
+	cmd_path = find_cmd_path(arg_arr[0], envp);
+	execve(cmd_path, arg_arr, envp);
 }
 
 int	main(int argc, char *argv[], char *envp[])
 {
-	char	*cmd_path;
+	int	id;
+	int	pipefd[2];
 
-	cmd_path = find_cmd_path(argv[1], envp);
-	printf("%s\n", cmd_path);
+	if (pipe(pipefd) == -1)
+		return (-1);
+	if (argc > 2)
+		id = fork();
+	if (id == 0)
+	{
+		close(pipefd[0]);
+		pipex_process(&pipefd[1], argv[1], envp);
+	}
+	else
+	{
+		close(pipefd[1]);
+		wait(NULL);
+		pipex_process(&pipefd[0], argv[2], envp);
+	}
 	return (0);
 }
--- pipex.h
+++ pipex.h
@@ -5,4 +5,14 @@
 
 size_t	find_path_env(char *str);
 
+char	**ft_split(char const *s, char c);
+
+char	*ft_strjoin(char const *s1, char const *s2);
+
+char	*ft_strjoin_free(char *s1, char *s2);
+
+void	*ft_calloc(size_t count, size_t size);
+
+char	*ft_strdup(const char *s1);
+
 #endif
